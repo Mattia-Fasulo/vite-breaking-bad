71874c1d1b21847a0807d5466174d20d8940d248
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-11-14 16:28:00 +0100

    add scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <AppHeader />
+  <SearchComponent />
+  <AppMain />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<script>
+import AppHeader from './assets/components/AppHeader.vue'
+import AppMain from './assets/components/AppMain.vue';
+import SearchComponent from './assets/components/SearchComponent.vue';
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    SearchComponent
   }
 }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
